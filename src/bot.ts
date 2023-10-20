af5f28514df7f6eb00b365bc3425bb11eaa3cf7d
Roberto Guglielmin <roberto@dvrs.studio>
2023-10-20 11:38:18 +0200

    Set up for prod and cleanup

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,5 @@
 import { Bot, Context, session, webhookCallback } from 'grammy';
 import commands from './commands/composer';
-import * as dotenv from 'dotenv';
-import { development } from './development';
 import {
     type Conversation,
     type ConversationFlavor,
@@ -14,9 +12,7 @@ import express, { json } from 'express';
 type MyContext = Context & ConversationFlavor;
 type MyConversation = Conversation<MyContext>;
 
-dotenv.config();
 const BOT_TOKEN = process.env.BOT_TOKEN!;
-const ENVIRONMENT = process.env.NODE_ENV!;
 
 const bot = new Bot<MyContext>(BOT_TOKEN);
 
@@ -31,8 +27,6 @@ bot.use(conversations());
 bot.use(createConversation(leaderboard));
 
 bot.use(commands);
-
-// ENVIRONMENT !== 'production' && development(bot);
 
 const server = express();
 server.use(json())
