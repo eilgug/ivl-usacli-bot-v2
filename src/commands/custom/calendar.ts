e848bf6b25e9eb4fb64979461c29e8fd2fdfb273
Roberto Guglielmin <roberto@dvrs.studio>
2023-10-27 09:46:29 +0200

    Update calendar command with settings

--- src/commands/custom/calendar.ts
+++ src/commands/custom/calendar.ts
@@ -7,6 +7,7 @@ import { getGroups } from '../../external/group';
 import { getCalendarMessage, getLeaderboardMessage } from '../../utils/helper';
 import { getTeamsByGroup } from '../../external/team';
 import { getCalendar } from '../../external/calendar';
+import { getChatSettings } from '../../external/settings';
 
 type MyContext = Context & ConversationFlavor;
 type MyConversation = Conversation<MyContext>;
@@ -14,50 +15,68 @@ type MyConversation = Conversation<MyContext>;
 const calendar = async (conversation: MyConversation, ctx: MyContext) => {
     const chatId = ctx.chat?.id!;
 
-    const territories = await getTerritories(conversation);
-    const {
-        selectedItem: territoryId,
-        replyMessage,
-        messageId: territoryMessageId,
-    } = await selectItem(conversation, ctx, 'Seleziona un territorio:', territories, 'ter');
+    const settings = await getChatSettings(conversation, chatId.toString());
 
-    const championships = await getChampionships(conversation, territoryId);
-    const { selectedItem: championshipId, messageId: championshipMessageId } = await selectItem(
-        conversation,
-        ctx,
-        'Seleziona un campionato:',
-        championships,
-        'cha',
-        chatId,
-        territoryMessageId,
-    );
+    if (!settings) {
+        const territories = await getTerritories(conversation);
+        const {
+            selectedItem: territoryId,
+            replyMessage,
+            messageId: territoryMessageId,
+        } = await selectItem(conversation, ctx, 'Seleziona un territorio:', territories, 'ter');
 
-    const groups = await getGroups(conversation, territoryId, championshipId);
-    const { selectedItem: groupId, messageId: groupMessageId } = await selectItem(
-        conversation,
-        ctx,
-        'Seleziona un girone:',
-        groups,
-        'gru',
-        chatId,
-        championshipMessageId,
-    );
+        const championships = await getChampionships(conversation, territoryId);
+        const { selectedItem: championshipId, messageId: championshipMessageId } = await selectItem(
+            conversation,
+            ctx,
+            'Seleziona un campionato:',
+            championships,
+            'cha',
+            chatId,
+            territoryMessageId,
+        );
 
-    const teams = await getTeamsByGroup(conversation, groupId);
-    const {selectedItem: teamId, messageId: teamMessageId} = await selectItem(
-        conversation,
-        ctx,
-        'Seleziona una squadra:',
-        teams,
-        'tea',
-        chatId,
-        groupMessageId
-    )
+        const groups = await getGroups(conversation, territoryId, championshipId);
+        const { selectedItem: groupId, messageId: groupMessageId } = await selectItem(
+            conversation,
+            ctx,
+            'Seleziona un girone:',
+            groups,
+            'gru',
+            chatId,
+            championshipMessageId,
+        );
 
-    const calendar = (await getCalendar(conversation, territoryId, championshipId, groupId, teamId)).rows;
+        const teams = await getTeamsByGroup(conversation, groupId);
+        const { selectedItem: teamId, messageId: teamMessageId } = await selectItem(
+            conversation,
+            ctx,
+            'Seleziona una squadra:',
+            teams,
+            'tea',
+            chatId,
+            groupMessageId,
+        );
+        const calendar = (
+            await getCalendar(conversation, territoryId, championshipId, groupId, teamId)
+        ).rows;
 
-    const message = getCalendarMessage(calendar, teamId);
-    await ctx.api.editMessageText(chatId, teamMessageId, message, { parse_mode: 'MarkdownV2' });
+        const message = getCalendarMessage(calendar, teamId);
+        await ctx.api.editMessageText(chatId, teamMessageId, message, { parse_mode: 'MarkdownV2' });
+    } else {
+        const calendar = (
+            await getCalendar(
+                conversation,
+                settings.territoryId,
+                settings.championshipId,
+                settings.groupId,
+                settings.teamId,
+            )
+        ).rows;
+
+        const message = getCalendarMessage(calendar, settings.teamId);
+        await ctx.reply(message, { parse_mode: 'MarkdownV2' });
+    }
 };
 
 export { calendar };
